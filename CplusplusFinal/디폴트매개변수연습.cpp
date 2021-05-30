d6594898feefd7d5e9fdceea176229b4f2f6fe27
jimin <jjimini98@naver.com>
2021-05-30 20:34:29 +0900

    0530

---
+++ CplusplusFinal/디폴트매개변수연습.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// 아래와 같이 하면 우리가 원하는 결과가 나오지 않음; 
+
+//void f() {
+//	cout << ' ' * 10 << endl; }
+//
+//void f(char s) {
+//	cout << s * 10 << endl; }
+//
+//
+//void f(char s, int n) {
+//	for (int i = 0; i < n; i++)
+//		cout << s * 10 << endl ;  }
+//
+//
+//
+//
+//void main() {
+//	f(); 
+//	f('@');
+//	f('@', 5);
+//}
+
+
+// 모범답안 
