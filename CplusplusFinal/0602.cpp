2ce375a0c39e5660548a7170e6ae0f0918c36fcf
jimin <jjimini98@naver.com>
2021-06-04 03:28:08 +0900

    finished 0602.cpp

--- CplusplusFinal/0602.cpp
+++ CplusplusFinal/0602.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int sum = 0; 
-
-	[&sum](int x, int y) {sum = x + y; }(2, 3);
-	cout << " 합은" << sum;
-
-
-
-
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int sum = 0; 
+//
+//	[&sum](int x, int y) {sum = x + y; }(2, 3);
+//	cout << " 합은" << sum;
+//
+//
+//
+//
+//}
